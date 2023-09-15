090bd088ac27d059c9c11ce547483e51029d9d38
seung-heee <tmdgmldi2595@naver.com>
2023-09-15 21:58:49 +0900

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
 import './css/font.css'
 
+
 function App() {
   return (
     <div className="App">
       <div className='chineseFont'>&#27874;&#21205;</div>
-
-      <div className='NanumSquareEB'>나눔스퀘어EB</div>
-      <div className='NanumGothicEB'>고딕EB</div>
-
-      <div className='NanumSquareB'>나눔스퀘어B</div>
-      <div className='NanumGothicB'>고딕B</div>
-
-      <div className='NanumSquareR'>나눔스퀘어R</div>
-      <div className='NanumGothicR'>고딕R</div>
-
-      <div className='NanumSquareL'>나눔스퀘어L</div>
-      <div className='NanumGothicL'>고딕L</div>
-
     </div>
   );
 }
