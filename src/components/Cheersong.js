f8e9924359c6a710abaa4c0edd6692c1825cc54b
hjklog <hjklog98@likelion.org>
2023-09-23 00:36:50 +0900

    Cheers.js 및 관련 컴포넌트 완료

---
+++ src/components/Cheersong.js
@@ -0,0 +1,53 @@
+const Cheersong = () => {
+    return (
+        <>
+        <div className="pageTitleCenter">
+                        <p className="mb-2 pt-8">성결대학교</p>
+                        <p>응원가</p>
+                    </div>
+                    <div className="text-center text-xs NanumSquareR pb-8">응원가는 인스타그램 @sku_39th_renew에서<br />감상하실 수 있습니다.</div>
+                    <div className="text-center text-xs NanumSquareL pb-8">
+                        던져 내 몸을 이 심장으로<br />
+                        저기 높은 하늘을 보며<br />
+                        신발끈을 동여매 검정에서<br />
+                        밝은 빛을 따라가다보면<br />
+                        내 맘은 저 높이 태양에 솟지<br />
+                        매일 하루 항해하는 나를 보며<br />
+                        이 세상을 걷지 더 위로 돌진<br />
+                        우리의 다음을 더욱 바라보며<br />
+                        <br />
+                        정신 없이 던지었던 날도<br />
+                        거침 없이 발을 딛어 날아올라<br />
+                        함성 속 yes I can fly<br />
+                        돌진하여 추월해 저 구름까지도<br />
+                        여전히 타올라<br />
+                        burning heart<br />
+                        (burning heart)<br />
+                        burning heart<br />
+                        (던져봐 나의 몸)<br />
+                        <br />
+                        그시간 속 우리 시간들이<br />
+                        지금 빛을 내고 있어<br />
+                        추억들이 우리를 동여매줘<br />
+                        이제 우린 함께인거야<br />
+                        너와난 더 높이 하늘을 날지<br />
+                        매일 같이 항해했던 우릴 보며<br />
+                        이세상을 다시 또 한번 돌진<br />
+                        우리의 내일을 위해 새롭게<br />
+                        <br />
+                        정신 없이 던지었던 날도<br />
+                        거침 없이 발을 딛어 날아올라<br />
+                        <br />
+                        함성 속 yes I can fly<br />
+                        돌진하여 추월해 저 구름까지도<br />
+                        여전히 타올라<br />
+                        burning heart<br />
+                        (burning heart)<br />
+                        burning heart<br />
+                        (던져봐 나의 몸)
+                    </div>
+                    </>
+    )
+}
+
+export default Cheersong;
